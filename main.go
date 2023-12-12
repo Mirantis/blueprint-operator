2cb01a3359fa76ffaa6c8fa91eae9795cfbe52a8
sakshisharma84 <sakshi.sharma.84@gmail.com>
2023-12-12 16:53:03 -0500

    add support for validation webhook

--- main.go
+++ main.go
@@ -110,6 +110,10 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Manifest")
 		os.Exit(1)
 	}
+	if err = (&boundlessv1alpha1.Blueprint{}).SetupWebhookWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create webhook", "webhook", "Blueprint")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
