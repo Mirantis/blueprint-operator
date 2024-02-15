e75d08a3dcd9e7a9c10cb70c8db41cd154e5db46
Ranyodh Singh <rsingh@mirantis.com>
2024-02-15 16:16:57 -0500

    Remove spec.components.core and ingress controller

--- main.go
+++ main.go
@@ -95,13 +95,6 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Blueprint")
 		os.Exit(1)
 	}
-	if err = (&controllers.IngressReconciler{
-		Client: mgr.GetClient(),
-		Scheme: mgr.GetScheme(),
-	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "Ingress")
-		os.Exit(1)
-	}
 	if err = (&controllers.ManifestReconciler{
 		Client:   mgr.GetClient(),
 		Scheme:   mgr.GetScheme(),
