84f182fe2de3e46bd6a1afd80396416df70ec97b
sakshisharma84 <sakshi.sharma.84@gmail.com>
2024-02-21 17:34:12 -0500

    add a function for the common code

--- api/v1alpha1/blueprint_webhook.go
+++ api/v1alpha1/blueprint_webhook.go
@@ -41,70 +41,49 @@ var _ webhook.Validator = &Blueprint{}
 // ValidateCreate implements webhook.Validator so a webhook will be registered for the type
 func (r *Blueprint) ValidateCreate() (admission.Warnings, error) {
 	blueprintlog.Info("validate create", "name", r.Name)
-
-	for _, val := range r.Spec.Components.Addons {
-		if strings.EqualFold(kindChart, val.Kind) {
-			if val.Manifest != nil {
-				blueprintlog.Info("received manifest object.", "Kind", kindChart)
-				return nil, fmt.Errorf("manifest object is not allowed for addon kind %s", kindChart)
-			}
-			if val.Chart == nil {
-				blueprintlog.Info("received empty chart object.", "Kind", kindChart)
-				return nil, fmt.Errorf("chart object can't be empty for addon kind %s", kindChart)
-			}
-		}
-
-		if strings.EqualFold(kindManifest, val.Kind) {
-			if val.Chart != nil {
-				blueprintlog.Info("received chart object.", "Kind", kindManifest)
-				return nil, fmt.Errorf("chart object is not allowed for addon kind %s", kindManifest)
-			}
-			if val.Manifest == nil {
-				blueprintlog.Info("received empty manifest object.", "Kind", kindManifest)
-				return nil, fmt.Errorf("manifest object can't be empty for addon kind %s", kindManifest)
-			}
-		}
-
-	}
-
-	return nil, nil
+	return validate(r.Spec)
 }
 
 // ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
 func (r *Blueprint) ValidateUpdate(old runtime.Object) (admission.Warnings, error) {
 	blueprintlog.Info("validate update", "name", r.Name)
-
-	for _, val := range r.Spec.Components.Addons {
-		if strings.EqualFold(kindChart, val.Kind) {
-			if val.Manifest != nil {
-				blueprintlog.Info("received manifest object.", "Kind", kindChart)
-				return nil, fmt.Errorf("manifest object is not allowed for addon kind %s", kindChart)
-			}
-			if val.Chart == nil {
-				blueprintlog.Info("received empty chart object.", "Kind", kindChart)
-				return nil, fmt.Errorf("chart object can't be empty for addon kind %s", kindChart)
-			}
-		}
-
-		if strings.EqualFold(kindManifest, val.Kind) {
-			if val.Chart != nil {
-				blueprintlog.Info("received chart object.", "Kind", kindManifest)
-				return nil, fmt.Errorf("chart object is not allowed for addon kind %s", kindManifest)
-			}
-			if val.Manifest == nil {
-				blueprintlog.Info("received empty manifest object.", "Kind", kindManifest)
-				return nil, fmt.Errorf("manifest object can't be empty for addon kind %s", kindManifest)
-			}
-		}
-
-	}
-
-	return nil, nil
+	return validate(r.Spec)
 }
 
 // ValidateDelete implements webhook.Validator so a webhook will be registered for the type
 func (r *Blueprint) ValidateDelete() (admission.Warnings, error) {
 	blueprintlog.Info("validate delete", "name", r.Name)
+
+	return nil, nil
+}
+
+func validate(spec BlueprintSpec) (admission.Warnings, error) {
+	if len(spec.Components.Addons) > 0 {
+		for _, val := range spec.Components.Addons {
+			if strings.EqualFold(kindChart, val.Kind) {
+				if val.Manifest != nil {
+					blueprintlog.Info("received manifest object.", "Kind", kindChart)
+					return nil, fmt.Errorf("manifest object is not allowed for addon kind %s", kindChart)
+				}
+				if val.Chart == nil {
+					blueprintlog.Info("received empty chart object.", "Kind", kindChart)
+					return nil, fmt.Errorf("chart object can't be empty for addon kind %s", kindChart)
+				}
+			}
+
+			if strings.EqualFold(kindManifest, val.Kind) {
+				if val.Chart != nil {
+					blueprintlog.Info("received chart object.", "Kind", kindManifest)
+					return nil, fmt.Errorf("chart object is not allowed for addon kind %s", kindManifest)
+				}
+				if val.Manifest == nil {
+					blueprintlog.Info("received empty manifest object.", "Kind", kindManifest)
+					return nil, fmt.Errorf("manifest object can't be empty for addon kind %s", kindManifest)
+				}
+			}
+
+		}
+	}
 
 	return nil, nil
 }
