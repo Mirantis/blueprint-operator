46709ffe4193d09e540b61cc59347172e48866ea
sakshisharma84 <sakshi.sharma.84@gmail.com>
2024-02-23 16:01:03 -0500

    add label prefix with mirantis domain

--- pkg/kustomize/kustomize.go
+++ pkg/kustomize/kustomize.go
@@ -32,7 +32,7 @@ func Render(logger logr.Logger, url string, values *boundlessv1alpha1.Values) ([
 	// This shall add the following label to all manifest objects
 	label := kustypes.Label{
 		Pairs: map[string]string{
-			"controlled-by": "boundless",
+			"controlled-by": "com.mirantis.boundless",
 		},
 		IncludeSelectors: true,
 	}
