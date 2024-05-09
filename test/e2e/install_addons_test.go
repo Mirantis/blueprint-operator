86160a6bf39f3374ad474a1a3b49224d11e9f268
Ranyodh Singh <rsingh@mirantis.com>
2024-05-09 11:27:35 -0400

    skip another e2e test

--- test/e2e/install_addons_test.go
+++ test/e2e/install_addons_test.go
@@ -9,7 +9,6 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/e2e-framework/pkg/features"
 
-	"github.com/mirantiscontainers/boundless-operator/api/v1alpha1"
 	"github.com/mirantiscontainers/boundless-operator/pkg/consts"
 	"github.com/mirantiscontainers/boundless-operator/test/e2e/funcs"
 )
@@ -43,7 +42,10 @@ func TestInstallAddons(t *testing.T) {
 		//funcs.AddonHaveStatusWithin(2*time.Minute, newAddon(a1), v1alpha1.TypeComponentAvailable),
 		)).
 		Assess("ManifestAddonIsSuccessfullyInstalled", funcs.AllOf(
-			funcs.AddonHaveStatusWithin(2*time.Minute, newAddon(a2), v1alpha1.TypeComponentAvailable),
+		// TODO (ranyodh): Skipping this check for now.
+		//  Re-enable with https://mirantis.jira.com/browse/BOP-589
+
+		//funcs.AddonHaveStatusWithin(2*time.Minute, newAddon(a2), v1alpha1.TypeComponentAvailable),
 		)).
 		Assess("HelmAddonObjectsAreSuccessfullyCreated", funcs.AllOf(
 			// @TODO: check for more/all objects
